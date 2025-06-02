97af7b9342391049c45d060ee9a99f749b565232
dwictor0 <daniel.wdev0@gmail.com>
2025-06-02 14:04:41 -0300

    FIX: ADICIONANDO_FILLABLE_MODEL

--- app/Models/ListaTarefas.php
+++ app/Models/ListaTarefas.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ListaTarefas extends Model
 {
-    //
+    protected $table = 'lista_tarefas';
+
+    protected $fillable = [
+        'titulo','descricao','status','created_at'
+    ];
 }
