c33c41194419417ada7f1f5744f1a33fd2972718
dwictor0 <daniel.wdev0@gmail.com>
2025-06-02 15:42:54 -0300

    FIX: ALTERANDO_ROTAS_WEB

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListaDeTarefasController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [ListaDeTarefasController::class, 'index'])->name('index.tarefas')->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
+    Route::resource('tarefas', ListaDeTarefasController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
