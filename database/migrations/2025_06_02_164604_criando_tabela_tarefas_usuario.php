f6a7bd4dd9fc9d9cc373ce63a9b910e13b3149ac
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 05:15:55 -0300

    FIX: DEFAULT_NULLABLE_COLUNA_DESCRICAO

--- database/migrations/2025_06_02_164604_criando_tabela_tarefas_usuario.php
+++ database/migrations/2025_06_02_164604_criando_tabela_tarefas_usuario.php
@@ -14,7 +14,7 @@ return new class extends Migration
       Schema::create('lista_tarefas', function (Blueprint $table) {
        $table->id();
        $table->text('titulo');
-       $table->text('descricao');
+       $table->text('descricao')->nullable();
        $table->integer('user_id')->nullable();
        $table->enum('status', ['pendente', 'concluida'])->default('pendente');
        $table->dateTime('created_at');
