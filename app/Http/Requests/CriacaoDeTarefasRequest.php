3675c421c2c2b455a353332662e3201c7ff41a43
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 05:14:42 -0300

    FIX: FORM_REQUEST_CRIACAO_TAREFAS

---
+++ app/Http/Requests/CriacaoDeTarefasRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CriacaoDeTarefasRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'titulo' => 'required',
+            'descricao' => 'sometimes',
+        ];
+    }
+    public function messages(): array
+    {
+        return[
+            'titulo.required' => 'Para realizar a criação de uma vaga informe o titulo!',
+        ];
+    }
+}
