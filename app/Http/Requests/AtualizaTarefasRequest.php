4dbea86bfd1c214961e8bcae6605b1d369ff82fc
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 13:36:12 -0300

    FIX: VALIDACAO_MAX_CARACTERES_ATUALIZA_TAREFA_REQUEST

--- app/Http/Requests/AtualizaTarefasRequest.php
+++ app/Http/Requests/AtualizaTarefasRequest.php
@@ -22,7 +22,7 @@ class AtualizaTarefasRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'titulo' => 'required',
+            'titulo' => 'required|max:255',
         ];
     }
 
