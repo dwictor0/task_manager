7033e775e56b19d05edab4d055d71dedb58d0959
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 13:20:19 -0300

    FEAT: ADICIONANDO_SOFT_DELETES_MODEL_TAREFAS

--- app/Models/ListaTarefas.php
+++ app/Models/ListaTarefas.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ListaTarefas extends Model
 {
+    use SoftDeletes;
     protected $table = 'lista_tarefas';
 
     protected $fillable = [
