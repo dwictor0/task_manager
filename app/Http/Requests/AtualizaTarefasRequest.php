17668b4e4e3dcb960f6aa8cb22cfb9de32e3a7a4
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 09:31:44 -0300

    FEAT: FORM_REQUEST_VALIDACAO_UPDATE

---
+++ app/Http/Requests/AtualizaTarefasRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AtualizaTarefasRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'titulo' => 'required',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+          'titulo.required'=> 'Não é possivel atualizar uma tarefa sem informar o titulo!.',
+        ];
+    }
+}
