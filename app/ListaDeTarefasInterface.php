0b07b8783e4c29a918e88fc522a83bf16471a674
dwictor0 <daniel.wdev0@gmail.com>
2025-06-04 09:15:50 -0300

    FIX: MELHORANDO_CODIGO_INTERFACE_TAREFAS

--- app/ListaDeTarefasInterface.php
+++ app/ListaDeTarefasInterface.php
@@ -2,14 +2,20 @@
 
 namespace App;
 
+use App\Http\Requests\AtualizaTarefasRequest;
+use App\Http\Requests\CriacaoDeTarefasRequest;
+use App\Models\ListaTarefas;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
 interface ListaDeTarefasInterface
 {
-    public function index();
-    public function create();
-    public function store(\App\Http\Requests\CriacaoDeTarefasRequest $request);
-    public function edit(\App\Models\ListaTarefas $tarefa);
-    public function update(\App\Http\Requests\AtualizaTarefasRequest $request, \App\Models\ListaTarefas $tarefa);
-    public function delete();
-    public function destroy($id);
-    public function restore($id);
+    public function index(): View;
+    public function create(): View;
+    public function store(CriacaoDeTarefasRequest $request): View|RedirectResponse;
+    public function edit(ListaTarefas $tarefa): View;
+    public function update(AtualizaTarefasRequest $request, ListaTarefas $tarefa): View|RedirectResponse;
+    public function IndexSoftDelete(): View;
+    public function destroy($id): View|RedirectResponse;
+    public function restore($id): View|RedirectResponse;
 }
