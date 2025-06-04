45231bf22e68912ad0f36b2c903e9ffd3b3a0de3
dwictor0 <daniel.wdev0@gmail.com>
2025-06-04 00:50:37 -0300

    FEAT: BLADE_ERRO_PERSONALIZADO

---
+++ resources/views/errors/exception.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Erro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+    <div class="container mt-5">
+        <div class="alert alert-danger" role="alert">
+            <h4 class="alert-heading">Ocorreu um erro!</h4>
+            <p>{{ $message }}</p>
+            <hr>
+        </div>
+    </div>
+
+</body>
+</html>
