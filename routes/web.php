df28ad52313ebf54caad2621e6e1cddaf5912117
dwictor0 <daniel.wdev0@gmail.com>
2025-06-04 00:49:15 -0300

    FEAT: TAREFAS_NOVA_ROTA_DELETED

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::get('/dashboard', [ListaDeTarefasController::class, 'index'])->middleware
 Route::middleware('auth')->group(function () {
     Route::resource('tarefas', ListaDeTarefasController::class);
     Route::post('/restore/{id}', [ListaDeTarefasController::class, 'restore'])->name('tarefas.restore');
+    Route::get('/deleted', [ListaDeTarefasController::class, 'delete'])->name('tarefas.delete');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
