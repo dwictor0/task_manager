1115a0a289b1cc4eae9b8d975870b6187f19223f
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 22:02:09 -0300

    FEAT: COMPONENTE_BLADE_BOTAO_RESTAURAR_TAREFAS

---
+++ resources/views/components/restore-button.blade.php
@@ -0,0 +1,7 @@
+<button
+    type="submit"
+    style="background-color: #22c55e; color: white;"
+    class="inline-flex items-center px-4 py-2 font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+>
+    Restaurar
+</button>
