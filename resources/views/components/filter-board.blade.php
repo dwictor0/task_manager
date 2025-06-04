7fdd605c21fa03238d69c693db431b1e93f8f25b
dwictor0 <daniel.wdev0@gmail.com>
2025-06-03 22:03:12 -0300

    FEAT: COMPONENTE_BLADE_FILTRO_TAREFAS

---
+++ resources/views/components/filter-board.blade.php
@@ -0,0 +1,11 @@
+<div class="mb-4 p-6">
+  <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-2">
+    Filtro por Status
+  </h2>
+
+  <select name="filtro_status" id="filtro_status" class="border rounded  py-1 text-black">
+    <option value="">Selecione</option>
+    <option value="Pendente">Pendente</option>
+    <option value="Concluida">Concluida</option>
+  </select>
+</div>
