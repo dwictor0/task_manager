492fe46932959da8686c1736b61b8a83f29163ad
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-03 20:33:37 -0300

    FIX: BOOTSTRAP_BUTTON_EXCLUIR_COMPONENTE

--- resources/views/components/trash-button.blade.php
+++ resources/views/components/trash-button.blade.php
@@ -1,6 +1,3 @@
-<button
-    type="submit"
-    class="inline-flex items-center px-4 py-2 bg-red-600 text-white font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
->
-    {{ $slot }}
+<button type="submit" class="btn btn-danger">
+{{ $slot }}
 </button>
