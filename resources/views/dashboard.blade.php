107864f8263a29bff0a3f5f02b4c524465db6aac
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-03 20:34:47 -0300

    FIX: MELHORANDO_CODIGO_DASHBOARD_BLADE

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,28 +3,52 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+  <h1 class="text-center">Gerenciamento de Tarefas</h1>
 @endsection
 
 @section('content')
-    <table class="table table-dark">
-  <thead>
-    ...
-  </thead>
-  <tbody>
-    <tr class="table-active">
-      ...
+  <table class="table table-dark">
+    <tr>
+    <td>Cód.Tarefa</td>
+    <td>Título</td>
+    <td>Descrição</td>
+    <td>Ações</td>
     </tr>
     <tr>
-      ...
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>John</td>
-      <td>Doe</td>
-      <td class="table-active">@social</td>
-    </tr>
-  </tbody>
-</table>
-    <p>Bem-vindo ao painel!</p>
+    @foreach ($tarefas as $tarefa)
+      <td>{{ $tarefa->id }}</td>
+
+      <td>{{ $tarefa->titulo }}</td>
+      <td>{{ $tarefa->descricao }}</td>
+      <td>
+      
+      <div class="d-flex align">
+        
+        <div class="trashbutton">
+        </div>
+        <div class="d-flex gap-3">
+
+        <div>
+        <a href="{{ route('tarefas.edit', ['tarefa' => $tarefa->id]) }}">
+        <x-edit-button>{{ __('Editar') }}</x-edit-button>
+        </a>
+        </div>
+
+        <div>
+
+        <form action="{{ route('tarefas.destroy', ['tarefa' => $tarefa->id]) }}" method="POST" class="inline">
+        @csrf
+        @method('DELETE') 
+        <x-trash-button>{{ __('Excluir') }}</x-trash-button>
+        </form>
+
+        </div>
+
+        </div>
+      </div>
+      
+      </td>
+      </tr>
+    @endforeach
+  </table>
 @endsection
