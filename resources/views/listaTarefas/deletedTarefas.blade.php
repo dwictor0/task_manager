398fa81403dd09430d210a03f6659caa7c6a77e1
dwictor0 <daniel.wdev0@gmail.com>
2025-07-02 22:40:00 -0300

    FIX: ALTERANDO_FOREACH_BLADE_TAREFAS_DELETED

--- resources/views/listaTarefas/deletedTarefas.blade.php
+++ resources/views/listaTarefas/deletedTarefas.blade.php
@@ -14,27 +14,27 @@
               </tr>
             </thead>
             <tbody>
-              @foreach ($indexTarefasDeleted as $index)
+              @foreach ($index as $indexTarefas)
 
             <tr class="bg-white dark:bg-gray-800 border rounded">
-            <td class="px-6 py-3">{{ $index->id }}</td>
-            @if(isset($index->deleted_at))
-          <td class="px-6 py-3 text-red-500">{{$index->titulo}} - DELETED</td>
+            <td class="px-6 py-3">{{ $indexTarefas->id }}</td>
+            @if(isset($indexTarefas->deleted_at))
+          <td class="px-6 py-3 text-red-500">{{$indexTarefas->titulo}} - DELETED</td>
         @else
-          <td class="px-6 py-3">{{$index->titulo}}</td>
+          <td class="px-6 py-3">{{$indexTarefas->titulo}}</td>
         @endif
-            @if($index->descricao != null)
-          <td class="px-6 py-3 whitespace-pre-line break-words">{{$index->descricao}}</td>
+            @if($indexTarefas->descricao != null)
+          <td class="px-6 py-3 whitespace-pre-line break-words">{{$indexTarefas->descricao}}</td>
         @else
           <td class="px-6 py-3 text-gray-500 italic">N/A</td>
         @endif
-            <td class="px-6 py-3 space-x-2">{{$index->status}}</td>
-            @if(isset($index->deleted_at))
-          <form action="{{ route('tarefas.restore', ['id' => $index->id]) }}" method="POST" class="inline">
+            <td class="px-6 py-3 space-x-2">{{$indexTarefas->status}}</td>
+            @if(isset($indexTarefas->deleted_at))
+          <form action="{{ route('tarefas.restore', ['id' => $indexTarefas->id]) }}" method="POST" class="inline">
           @csrf
           <td><x-restore-button></x-restore-button></td>
           </form>
-          <form action="{{ route('tarefas.destroy', ['tarefa' => $index->id]) }}" method="POST">
+          <form action="{{ route('tarefas.destroy', ['tarefa' => $indexTarefas->id]) }}" method="POST">
           @csrf
           @method('DELETE')
           <td><x-trash-button>
