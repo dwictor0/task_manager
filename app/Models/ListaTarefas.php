805ac0138bdd97c0112efae8d5ea11f979a5f685
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-04 13:57:21 -0300

    FIX: MELHORANDO_MODEL_LISTA_TAREFAS

--- app/Models/ListaTarefas.php
+++ app/Models/ListaTarefas.php
@@ -7,10 +7,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ListaTarefas extends Model
 {
-    use SoftDeletes;
-    protected $table = 'lista_tarefas';
-
-    protected $fillable = [
-        'titulo','descricao','status','created_at','user_id'
-    ];
+  use SoftDeletes;
+  protected $table = 'lista_tarefas';
+  protected $dates = [
+    'data_de_vencimento'
+  ];
+  protected $fillable = [
+    'titulo',
+    'descricao',
+    'status',
+    'created_at',
+    'user_id',
+    'prioridade',
+    'data_de_vencimento'
+  ];
+  protected $casts = [
+    'data_de_vencimento' => 'date',
+  ];
 }
