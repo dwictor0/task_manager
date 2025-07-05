fc39fdb04189daa96ee86c824ef775ee5a774079
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 01:44:24 -0300

    FEAT: ADICIONANDO_ALERTA_APP_PROVIDER

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Jobs\EnviarAlertaTarefaJob;
+use App\Services\TarefasService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->booted(function () {
+            app(Schedule::class)->everyMinute()->call(function () {
+
+                $tarefasProximas = app(TarefasService::class)->buscarTarefasProximas();
+
+                foreach ($tarefasProximas as $tarefa) {
+                    dispatch(new EnviarAlertaTarefaJob($tarefa->id));
+                }
+            });
+        });
     }
 
     /**
