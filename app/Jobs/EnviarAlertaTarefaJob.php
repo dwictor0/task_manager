1df92a7fe0e16d14b4b6c6619c02b4a333acdd41
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 11:23:37 -0300

    FEAT: JOBS_RETRY_E_TIMEOUT

--- app/Jobs/EnviarAlertaTarefaJob.php
+++ app/Jobs/EnviarAlertaTarefaJob.php
@@ -16,35 +16,41 @@ class EnviarAlertaTarefaJob implements ShouldQueue
     use Dispatchable, Queueable;
 
     protected $tarefaId;
+    public $tries = 3;
+    public $timeout = 200;
 
-    
     public function __construct($tarefaId)
     {
         $this->tarefaId = $tarefaId;
     }
 
-    
+
     public function handle(TarefasService $tarefasService)
     {
-   $tarefa = ListaTarefas::find($this->tarefaId);
-    if (!$tarefa) {
-        \Log::warning("Tarefa {$this->tarefaId} não encontrada.");
-        return;
+        $tarefa = ListaTarefas::find($this->tarefaId);
+        if (!$tarefa) {
+            \Log::warning("Tarefa {$this->tarefaId} não encontrada.");
+            return;
+        }
+
+
+        event(new TestePusherEvent($tarefa));
+
+
+        // Mail::to($tarefa->user->email)->send(new AlertaTarefaMail($tarefa));
+
+
+        $tarefa->alerta_enviado = true;
+        $tarefa->update(['alerta_enviado' => true]);
+
+
+        \Log::info("Alerta enviado para tarefa {$this->tarefaId}.");
+
     }
 
-
-    event(new TestePusherEvent($tarefa));
-
-
-    // Mail::to($tarefa->user->email)->send(new AlertaTarefaMail($tarefa));
-
-
-    $tarefa->alerta_enviado = true;
-    $tarefa->update(['alerta_enviado' => true]);
-
-
-    \Log::info("Alerta enviado para tarefa {$this->tarefaId}.");
-    
+    public function failed(Exception $exception)
+    {
+        \Log::error("Job falhou: " . $exception->getMessage());
     }
 }
 
