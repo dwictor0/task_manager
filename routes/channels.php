2240e06bd9691b414913db82f073915d56f786b0
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 09:00:02 -0300

    FEAT: ADICIONANDO_CHANNEL_PARA_PUSHER_EVENT

--- routes/channels.php
+++ routes/channels.php
@@ -7,6 +7,8 @@ Broadcast::channel('chat', function () {
 });
 
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+
+Broadcast::channel('usuario.{userId}', function ($user, $userId) {
+    return (int) $user->id === (int) $userId;
 });
+
