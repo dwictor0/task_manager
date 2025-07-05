88ebe2bd6bb70686fb4df21b6ee43ab420942550
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 01:46:19 -0300

    FEAT: NOVO_COMANDO_ARTISAN_VERIFICA_TAREFAS

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\VerificarTarefasVencimento;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('app:verificar-tarefas-vencimento', function () {
+    $this->call(VerificarTarefasVencimento::class);
+})->describe('Verifica tarefas próximas do vencimento e dispara alertas');
