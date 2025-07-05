cabca1614344342718f5afcfd633433a76672fa3
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 09:14:09 -0300

    FEAT: VIEW_BLADE_CONTROLE_DE_TAREFAS

---
+++ resources/views/listaTarefas/controleTarefas.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+  <h1 class="text-center">Gerenciamento de Tarefas</h1>
+ 
+    <button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+
+
+
+@endsection
+
+@section('content')
+
+@endsection
