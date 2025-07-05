cac39c7af0844620900c851941c2a72715f9d57c
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 18:40:56 -0300

    FIX: CORRIGINDO_TIPO_DE_DADOS_MIGRATION

--- database/migrations/2025_06_02_164604_criando_tabela_tarefas_usuario.php
+++ database/migrations/2025_06_02_164604_criando_tabela_tarefas_usuario.php
@@ -13,13 +13,12 @@ return new class extends Migration
     {
       Schema::create('lista_tarefas', function (Blueprint $table) {
        $table->id();
-       $table->text('titulo');
+       $table->string('titulo');
        $table->text('descricao')->nullable();
        $table->integer('user_id')->nullable();
        $table->enum('status', ['pendente', 'concluida','em_progresso'])->default('pendente');
-       $table->dateTime('created_at');
-       $table->dateTime('updated_at');
-       $table->dateTime('deleted_at')->nullable();
+       $table->timestamps();
+       $table->softDeletes();
       });
     }
 
