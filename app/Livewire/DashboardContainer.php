947c31c8724728d6f803984751c12db7929d345c
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-05 12:59:10 -0300

    FEAT: CONFIGURANDO_ESCUTA_EVENT_PUSHER_EM_COMPONENTE_LIVEWIRE

--- app/Livewire/DashboardContainer.php
+++ app/Livewire/DashboardContainer.php
@@ -10,14 +10,25 @@ class DashboardContainer extends Component
 {
     public $tarefas;
 
-    public function render()
-    {
-        return view('livewire.dashboard-container', ['tarefas' => $this->tarefas]);
-    }
+    protected $listeners = ['evento' => 'atualizarTarefas'];
+
+    protected $tarefasService;
 
     public function mount(TarefasService $tarefas)
     {
-        $this->tarefas = $tarefas->indexTarefas();
+        $this->tarefasService = $tarefas;
+        $this->atualizarTarefas();
+    }
 
+
+    public function atualizarTarefas()
+    {
+        $this->tarefas = app(\App\Services\TarefasService::class)->indexTarefas();
+    }
+
+
+    public function render()
+    {
+        return view('livewire.dashboard-container', ['tarefas' => $this->tarefas]);
     }
 }
