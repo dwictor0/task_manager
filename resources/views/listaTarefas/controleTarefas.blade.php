c75c593060a752c842e4df7e010d0060a9596986
ti.upa1 <daniel.wdev0@gmail.com>
2025-07-06 08:32:25 -0300

    FIX: CORRIGINDO_NOME_DE_VARIAVEIS_BLADE_CONTROLE

--- resources/views/listaTarefas/controleTarefas.blade.php
+++ resources/views/listaTarefas/controleTarefas.blade.php
@@ -11,10 +11,9 @@
 
 @section('content')
 
-<input type="hidden" name="prioridade_alta" id="prioridade_alta" value="{{ $totaisPrioridade['alta'] ?? 0  }}">
-<input type="hidden" name="prioridade_media" id="prioridade_media" value="{{  $totaisPrioridade['media'] ?? 0  }}">
-<input type="hidden" name="prioridade_baixa" id="prioridade_baixa" value="{{  $totaisPrioridade['baixa'] ?? 0  }}">
-
+<input type="hidden" name="prioridade_alta" id="prioridade_alta" value="{{ $totalTarefasPrioridade['alta'] ?? 0  }}">
+<input type="hidden" name="prioridade_media" id="prioridade_media" value="{{  $totalTarefasPrioridade['media'] ?? 0  }}">
+<input type="hidden" name="prioridade_baixa" id="prioridade_baixa" value="{{  $totalTarefasPrioridade['baixa'] ?? 0  }}">
 
 <div class="card">
   <div class="card-header">
@@ -31,7 +30,7 @@
 
   <div class="info-box-content">
     <span class="info-box-text">Tarefas Pendentes</span>
-    <span class="info-box-number">{{ $totaisStatus['pendente'] ?? 0 }}</span>
+    <span class="info-box-number">{{ $totalTarefasStatus['pendente'] ?? 0 }}</span>
   </div>
 </div>
 <div class="info-box">
@@ -39,7 +38,7 @@
  
   <div class="info-box-content">
     <span class="info-box-text">Tarefas Concluidas</span>
-    <span class="info-box-number">{{ $totaisStatus['concluida'] ?? 0 }}</span>
+    <span class="info-box-number">{{ $totalTarefasStatus['concluida'] ?? 0 }}</span>
   </div>
 </div>
 
@@ -48,17 +47,16 @@
 
   <div class="info-box-content">
     <span class="info-box-text">Tarefas Em Andamento</span>
-    <span class="info-box-number">{{ $totaisStatus['em_progresso'] ?? 0 }}</span>
+    <span class="info-box-number">{{ $totalTarefasStatus['em_progresso'] ?? 0 }}</span>
   </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <script>
   const ctx = document.getElementById('tarefasChart').getContext('2d');
-  let a  = $('#prioridade_alta').val();
-  let b  = $('#prioridade_media').val();
-  let c  = $('#prioridade_baixa').val();
-  console.log(a);
+  let tarefaPrioridadeAlta = $('#prioridade_alta').val();
+  let tarefaPrioridadeMedia  = $('#prioridade_media').val();
+  let tarefaPrioridadeBaixa  = $('#prioridade_baixa').val();
   
   const tarefasChart = new Chart(ctx, {
     type: 'bar', // pode ser 'line', 'pie', etc.
@@ -66,7 +64,7 @@
       labels: ['Alta', 'Média', 'Baixa'], // categorias
       datasets: [{
         label: 'Quantidade de Tarefas',
-        data: [a, b, c], // números que representam os dados
+        data: [tarefaPrioridadeAlta, tarefaPrioridadeMedia, tarefaPrioridadeBaixa], // números que representam os dados
         backgroundColor: [
           'rgba(220, 53, 69, 0.7)', // vermelho (alta)
           'rgba(255, 193, 7, 0.7)',  // amarelo (média)
