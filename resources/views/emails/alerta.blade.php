c309f1096e876c17ecbdb57b9d273c531b464c1a
dwictor0 <daniel.wdev0@gmail.com>
2025-07-08 21:13:35 -0300

    FIX: ALTERANDO_TITLE_ALERTA_BLADE_MAIL

--- resources/views/emails/alerta.blade.php
+++ resources/views/emails/alerta.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Email</title>
+    <title>Gerenciamento de Tarefas</title>
 </head>
 <body>
 <h1>Olá, Usuário #{{ $userId }}</h1>
