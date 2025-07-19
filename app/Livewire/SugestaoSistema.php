41edc61324e4913f3b308fa39e3d6abb90ffc87c
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 13:00:37 -0300

    FEAT: COMPONENTE_PHP_LIVEWIRE_SUGESTAO_SISTEMA

---
+++ app/Livewire/SugestaoSistema.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SugestaoSistema extends Component
+{
+    public function render()
+    {
+        return view('livewire.sugestao-sistema');
+    }
+}
