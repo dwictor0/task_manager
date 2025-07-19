be8f6684c678c9bc579497d78b3926efc9b04e17
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 12:47:57 -0300

    FEAT: BLADE_SUGESTAO_INDEX

---
+++ resources/views/sugestao/indexSugestao.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('title', 'System - Tarefas')
+
+@section('content_header')
+  <h1 class="text-center">Envie sua Sugestão</h1>
+ @if($errors->any())
+  <div class="alert alert-danger alert-dismissible fade show shadow-sm rounded" role="alert">
+    <strong>
+      <i class="fas fa-exclamation-circle me-2"></i> Erro!
+    </strong>
+  
+      @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    <button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+@endif
+
+
+@endsection
+
+@section('content')
+ 
+@endsection
