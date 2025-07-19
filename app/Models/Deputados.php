ea9df185aa1b129d4bd0bfc8d098f554b6fa69f3
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 12:31:25 -0300

    FEAT: CRIANDO_MODEL_DEPUTADOS

---
+++ app/Models/Deputados.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Deputados extends Model
+{
+  protected $table = 'deputados';
+    
+  protected $fillable = [
+    'nome',
+    'partido',
+    'uf',
+    'id_api',
+  ];
+
+}
