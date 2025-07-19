2c2998c0f4cb287bc10948f7848c0f4c9ad9f510
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 20:04:39 -0300

    FIX: REMOVENDO_COLUNA_TABELA_SUGESTAO

--- database/migrations/2025_07_19_130110_tabela_sugestao.php
+++ database/migrations/2025_07_19_130110_tabela_sugestao.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('titulo');
             $table->string('descricao')->nullable();
             $table->unsignedBigInteger('usuario_id');
-            $table->unsignedInteger('total_votos')->default(0); 
             $table->timestamps();
         });
     }
