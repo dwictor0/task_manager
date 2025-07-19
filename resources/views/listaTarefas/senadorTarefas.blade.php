6deb196f6bb838934d3e72c493bb320f231ea1ea
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 17:29:26 -0300

    FEAT: BLADE_SENADOR_TAREFAS_INDEX

---
+++ resources/views/listaTarefas/senadorTarefas.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'System - Tarefas')
+
+@section('content_header')
+    <h1 class="mb-4">Senadores</h1>
+@endsection
+
+@section('content')
+<livewire:senador-index></livewire:senador-index>   
+@endsection
