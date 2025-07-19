f3509765e423ee96ceea0b5c8e63183413c1211c
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 13:59:11 -0300

    FIX: ROUTES_WEB_SUGESTOES

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/sugestoes', [SugestoesController::class, 'index'])->name('sugestao.index');
+    
+    Route::get('/sugestoes', [SugestoesController::class, 'indexSugestoes'])->name('sugestao.index');
+    Route::get('/save/sugestoes', [SugestoesController::class, 'criarSugestoes'])->name('sugestao.save');
+    Route::post('/store/sugestoes', [SugestoesController::class, 'salvarSugestoes'])->name('sugestao.store');
 });
 
 require __DIR__ . '/auth.php';
