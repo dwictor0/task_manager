e984a8c4ec143f13b23bc476e27a2656f9457027
dwictor0 <daniel.wdev0@gmail.com>
2025-07-19 13:56:13 -0300

    FEAT: SUGESTAO_CONTROLLER

--- app/Http/Controllers/SugestoesController.php
+++ app/Http/Controllers/SugestoesController.php
@@ -2,12 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sugestao;
+use Auth;
 use Illuminate\Http\Request;
 
 class SugestoesController extends Controller
 {
-    public function index()
+    private $sugestao;
+
+    /**
+     * Summary of __construct
+     * @param \App\Models\Sugestao $sugestao
+     */
+    public function __construct(Sugestao $sugestao)
+    {
+        $this->sugestao = $sugestao;
+    }
+
+    /**
+     * Summary of indexSugestoes
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function indexSugestoes()
     {
         return view('sugestao.indexSugestao');
+    }
+
+    /**
+     * Summary of criarSugestoes
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function criarSugestoes(Request $request)
+    {
+        return view('sugestao.createSugestao');
+    }
+
+    /**
+     * Summary of salvarSugestoes
+     * @param \Illuminate\Http\Request $request
+     * @return mixed|\Illuminate\Http\RedirectResponse
+     */
+    public function salvarSugestoes(Request $request)
+    {
+        $userId = Auth::id();
+
+        $this->sugestao->create([
+            'titulo' => $request->titulo,
+            'descricao' => $request->descricao,
+            'usuario_id' => $userId,
+        ]);
+
+        return redirect()->route('sugestao.index');
+
     }
 }
