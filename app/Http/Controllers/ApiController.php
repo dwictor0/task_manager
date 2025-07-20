942634bd00251755a9d75bc88215f163359c2887
dwictor0 <daniel.wdev0@gmail.com>
2025-07-20 15:45:33 -0300

    FIX: MELHORANDO_QUALIDADE_DE_CODIGO_API_CONTROLLER

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,38 +1,66 @@
 <?php
 namespace App\Http\Controllers;
 use App\Models\Deputados;
+use DB;
+use Exception;
 use Illuminate\Support\Facades\Http;
+use Log;
 
 
 class ApiController extends Controller
 {
+    /**
+     * Summary of deputados
+     * @var 
+     */
+    private $deputados;
+
+    /**
+     * Summary of __construct
+     * @param \App\Models\Deputados $deputados
+     */
+    public function __construct(Deputados $deputados)
+    {
+        $this->deputados = $deputados;
+    }
+
+    /**
+     * Summary of renderizaApi
+     * @return void
+     */
     public function renderizaApi()
     {
-        $url = 'https://legis.senado.leg.br/dadosabertos/senador/lista/atual';
+        try {
+            if ($this->deputados->count() == 0) {
 
-        $response = Http::get($url);
-        $xmlString = $response->body();
-        $xml = simplexml_load_string($xmlString);
-        $json = json_encode($xml);
-        $array = json_decode($json, true);
-        $collect = collect($array);
-        $parlamentares = data_get($collect, 'Parlamentares.Parlamentar');
+                $url = 'https://dadosabertos.camara.leg.br/api/v2/deputados';
 
-        if (isset($parlamentares['IdentificacaoParlamentar'])) {
-            $parlamentares = [$parlamentares];
+                $response = Http::get($url);
+
+                if (!$response->successful()) {
+                    Log::error("Erro ao acessar a API dos deputados com a URL fornecida");
+                }
+
+                DB::beginTransaction();
+                
+                $dados = $response->json();
+                $deputados = $dados['dados'] ?? [];
+
+                foreach ($deputados as $dep) {
+                    $this->deputados->create(
+                        [
+                            'nome' => $dep['nome'],
+                            'partido' => $dep['siglaPartido'],
+                            'imagem_deputado' => $dep['urlFoto'],
+                            'uf' => $dep['siglaUf'],
+                        ],
+                    );
+                }
+                DB::commit();
+            }
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::error("Erro ao renderizar a API:{$e->getMessage()} | Linha: {$e->getLine()} | Trace: {$e->getTraceAsString()}");
         }
-        collect($parlamentares)->each(function ($parlamentar) {
-            $identificacao = $parlamentar['IdentificacaoParlamentar'] ?? [];
-
-            Deputados::create(
-                [
-                    'nome' => $identificacao['NomeParlamentar'] ?? null,
-                    'id_api' => $identificacao['CodigoParlamentar'] ?? null,
-                    'partido' => $identificacao['SiglaPartidoParlamentar'],
-                    'imagem_senador' => $identificacao['UrlFotoParlamentar']
-                ]
-            );
-        });
-
     }
 }
