a715a0a24d5c206ff88903f737a6262ce6707f9c
dwictor0 <daniel.wdev0@gmail.com>
2025-07-20 16:03:18 -0300

    FEAT: BLADE_DEPUTADO_TAREFAS

---
+++ resources/views/listaTarefas/deputadoTarefas.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'System - Tarefas')
+
+@section('content_header')
+    <h1 class="mb-4">Deputados</h1>
+@endsection
+
+@section('content')
+<livewire:deputado-index></livewire:deputado-index>  
+@endsection
