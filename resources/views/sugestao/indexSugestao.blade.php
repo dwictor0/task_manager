072b5817583c262b9c1bdb5be67a4b793c8cfaa1
dwictor0 <daniel.wdev0@gmail.com>
2025-07-20 16:37:45 -0300

    FEAT: FEEDBACK_DE_ERRO_BLADE_INDEX_SUGESTAO

--- resources/views/sugestao/indexSugestao.blade.php
+++ resources/views/sugestao/indexSugestao.blade.php
@@ -11,6 +11,21 @@
             </button>
         </div>
     @endif
+
+    @if($errors->any())
+  <div class="alert alert-danger alert-dismissible fade show shadow-sm rounded" role="alert">
+    <strong>
+      <i class="fas fa-exclamation-circle me-2"></i> Erro!
+    </strong>
+  
+      @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    <button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+@endif
 @endsection
 
 @section('content')
