0054aec2cd4f5a1f311aa6941953e0f42ad9a024
dwictor0 <daniel.wdev0@gmail.com>
2025-07-20 15:46:02 -0300

    FEAT: SALVANDO_IP_DO_USUARIO_AO_REGISTRAR

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'ip_address' => $request->ip(),
         ]);
 
         event(new Registered($user));
