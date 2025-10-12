c8e3a3ba18d67beef909661017b7905508d1e0f9
dwictor0 <daniel.wdev0@gmail.com>
2025-10-11 21:32:28 -0300

    FIX: REMOVENDO_COLUNAS_RELACIONAMENTO_INEXISTENTE_MIGRATE

--- database/migrations/2025_07_19_183235_relacionamento_tabela_tarefas.php
+++ database/migrations/2025_07_19_183235_relacionamento_tabela_tarefas.php
@@ -21,7 +21,7 @@ return new class extends Migration {
   public function down(): void
   {
     Schema::table('lista_tarefas', function (Blueprint $table) {
-      $table->dropConstrainedForeignId('deputado_id');
+      
     });
   }
 };
